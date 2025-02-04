bfdc94a06332145f101b186f6e2e08e19de0a457
Brad Bodine <bbodine1@gmail.com>
2025-02-04 12:22:04 -0600

    Add ESLint configuration and remove obsolete .eslintignore file

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+// eslint.config.mjs
+
+export default [
+  {
+    ignores: [
+      "**/node_modules/",
+      "dist/",
+      "coverage/",
+      ".husky/",
+      ".vscode/",
+      "public/",
+      ".yarn/"
+    ]
+  },
+];
